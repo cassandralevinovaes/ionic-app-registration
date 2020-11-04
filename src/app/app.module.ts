efabd90ec8e03b0c3a19f320fcfa08a4181e297f
cassandralevinovaes <cassandralevi@gmail.com>
2020-11-04 09:32:49 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [AppComponent, HomepageComponent, LoginComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
