822e41876ef856002cf448167cdf02e7890dacc0
cassandralevinovaes <cassandralevi@gmail.com>
2020-11-04 13:29:30 -0300

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,1 +1,14 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
+/**
+ * In this file we set up the main routes within our application
+ * We have multiple options available to us for routing
+ * 
+ * Route to a specific component
+ * Route to a lazy-loaded module
+ * Redirect to a predefined route 
+ * 
+ * We can see an example of each below
+ */
